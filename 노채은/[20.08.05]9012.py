eff739ea07bd99e5823f98cbe9d6608c38713676
NCE20 <nce0130@naver.com>
2020-08-05 23:02:00 +0900

    update

---
+++ 노채은/[20.08.05]9012.py
@@ -0,0 +1,19 @@
+#BOJ 9012
+
+n=int(input())
+for _ in range(n):
+    text=list(input())
+    arr=[]
+    for element in text:
+        if element=="(":
+            arr.append(element)
+        elif element==")":
+            if len(arr)==0:
+                arr.append(element)
+                break
+            else:
+                arr.pop()
+    if len(arr)>0:
+        print("NO")
+    else:
+        print("YES")
